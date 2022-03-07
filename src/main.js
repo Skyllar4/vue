31fe7416a1acca0d6941920c66a99d4c1865d2c1
Skyllar <vladimirprasolov613@gmail.com>
2022-03-07 21:17:19 +0300

    commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import foo from './func'
-import { str1, str2 } from './data'
+// import foo from './func'
+// import { str1, str2 } from './data'
 
 createApp(App).mount('#app')
 
-foo(str1)
-foo(str2)
+// foo(str1)
+// foo(str2)
