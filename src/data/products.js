a860070ef330954dc37e2389859dca9d6b815f94
Skyllar <vladimirprasolov613@gmail.com>
2022-05-09 20:04:21 +0300

    commit

--- src/data/products.js
+++ src/data/products.js
@@ -7,5 +7,26 @@ export default [{
         title: '123',
         price: '2000',
         image: "https://bipbap.ru/wp-content/uploads/2017/04/priroda_kartinki_foto_03.jpg"
-    }
+    }, {
+        title: 'Радионяня Motorola MBP16',
+        price: "2690",
+        image: '/img/radio.jpg'
+    }, {
+        title: 'Радионяня Motorola MBP16',
+        price: "2690",
+        image: '/img/radio.jpg'
+    }, {
+        title: 'Радионяня Motorola MBP16',
+        price: "2690",
+        image: '/img/radio.jpg'
+    }, {
+        title: 'Радионяня Motorola MBP16',
+        price: "2690",
+        image: '/img/radio.jpg'
+    },
+    {
+        title: 'Радионяня Motorola MBP16',
+        price: "2690",
+        image: '/img/radio.jpg'
+    },
 ]
